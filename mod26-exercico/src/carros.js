595034572e925c650630a7f0c25f22eb7bfdf846
Luciano Pereira <luciano.pereiraaristimunho@gmail.com>
2022-07-05 09:12:54 -0300

    versao1

---
+++ mod26-exercico/src/carros.js
@@ -0,0 +1,40 @@
+
+
+export const cars = [
+
+    {
+        id: 1,
+        marca: 'ford',
+        modelo: 'ka',
+        ano: 2013,
+        portas: 2,
+        cambio: 'manual',
+        combustivel: ['gasolina', 'etanol']
+    },
+    {
+        id: 2,
+        marca: 'ford',
+        modelo: 'ecosport',
+        ano: 2018,
+        portas: 4,
+        cambio: 'manual',
+        combustivel: ['gasolina', 'etanol']
+    },
+    {
+        id: 3,
+        marca: 'chevrolet',
+        modelo: 'tracker',
+        ano: 2023,
+        portas: 2,
+        cambio: 'automático',
+        combustivel: ['gasolina', 'etanol']
+    }
+];
+  
+
+
+
+  
+
+
+
